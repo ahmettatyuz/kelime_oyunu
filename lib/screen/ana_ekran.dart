78bfc055313a3969c4b838ffe95aea227eb703a9
Ahmet Tatyuz <ahmettatyuz@gmail.com>
2024-02-05 16:39:54 +0300

    timer düzeltildi

--- lib/screen/ana_ekran.dart
+++ lib/screen/ana_ekran.dart
@@ -1,8 +1,9 @@
-import 'dart:async';
 import 'dart:math';
+
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:kelime_oyunu/data.dart';
-import 'package:kelime_oyunu/widgets/harf_kutusu.dart';
+import 'package:kelime_oyunu/screen/oyun.dart';
 
 class AnaEkran extends StatefulWidget {
   const AnaEkran({super.key});
@@ -11,240 +12,29 @@ class AnaEkran extends StatefulWidget {
 }
 
 class _AnaEkranState extends State<AnaEkran> {
-  TextEditingController tahminController = TextEditingController();
-  int orjinalSure = 60;
-  int kalanSure = 60;
-  Timer? timer;
-  List kelime = [];
-  List gorunenKelime = [];
-  int hak = 5;
-  int tahminSayisi = 0;
-  List gorunenKelimeler = [];
-  bool tahminEdebilir = false;
-  bool sureyiBaslat = true;
-  String mesaj = "";
-  bool oyunBitti = false;
-  bool kazandiMi = false;
-
   String yeniKelime() {
     var randSayi = Random().nextInt(words.length);
     return words[randSayi];
   }
 
-  void oyunuYenile() {
-    setState(() {
-      kelime = yeniKelime().toUpperCase().split("");
-      kalanSure = orjinalSure;
-      gorunenKelime = [];
-      hak = 5;
-      tahminSayisi = 0;
-      gorunenKelimeler = [];
-      tahminEdebilir = false;
-      sureyiBaslat = true;
-      tahminController.text = "";
-      oyunBitti = false;
-      kazandiMi = false;
-      timer!.cancel();
-    });
-  }
-
-  void tahmin(String tahminKelime) {
-    print("tahmin edilen kelime $tahminKelime");
-    print("tahmin edilen kelime ${kelime.join()}");
-    for (int j = 0; j < kelime.length; j++) {
-      if (kelime[j] == tahminKelime[j]) {
-        gorunenKelimeler[tahminSayisi][j] = "+${tahminKelime[j]}";
-      } else {
-        gorunenKelimeler[tahminSayisi][j] = "-${tahminKelime[j]}";
-      }
-
-      if (kelime.contains(tahminKelime[j]) &&
-          !gorunenKelimeler[tahminSayisi].contains("+${tahminKelime[j]}")) {
-        gorunenKelimeler[tahminSayisi][j] = "?${tahminKelime[j]}";
-      }
-    }
-
-    if (kelime.join() == tahminKelime) {
-      mesaj = "TEBRİKLER";
-      oyunBitti = true;
-      kazandiMi = true;
-      timer!.cancel();
-    }
-
-    tahminSayisi++;
-    tahminController.text = "";
-    tahminEdebilir = false;
-  }
-
-  void startTimer() {
-    const oneSec = Duration(seconds: 1);
-    timer = Timer.periodic(
-      oneSec,
-      (Timer timer) {
-        if (kalanSure == 0) {
-          setState(() {
-            timer.cancel();
-            oyunBitti = true;
-          });
-        } else {
-          setState(() {
-            kalanSure--;
-            mesaj = "Kalan süre : $kalanSure";
-          });
-        }
-      },
-    );
-  }
-
-  List gorunenKelimeOlustur(int uzunluk) {
-    List gorunenKelime = [];
-    for (int i = 0; i < uzunluk; i++) {
-      gorunenKelime.add("  ");
-    }
-    return gorunenKelime;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    kelime = yeniKelime().toUpperCase().split("");
-  }
-
+  final CountDownController timerController = CountDownController();
   @override
   Widget build(BuildContext context) {
-    if (sureyiBaslat) {
-      sureyiBaslat = false;
-      startTimer();
-    }
-    if (!kazandiMi && (kalanSure == 0 || hak == 0)) {
-      mesaj = "KELİME ŞUYDU : ${kelime.join()}";
-      oyunBitti = true;
-      timer!.cancel();
-    }
-    print(kelime);
-    if (gorunenKelime.length != kelime.length) {
-      gorunenKelime = [kelime[0]];
-      for (int i = 1; i < kelime.length; i++) {
-        gorunenKelime.add("  ");
-      }
-    }
-    if (gorunenKelimeler.isEmpty) {
-      gorunenKelimeler.add(gorunenKelime);
-      for (int i = 1; i < hak; i++) {
-        gorunenKelimeler.add(gorunenKelimeOlustur(kelime.length));
-      }
-    }
-    print(gorunenKelimeler);
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Column(children: [
-            ...gorunenKelimeler.map(
-              (e) => Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [...e.map((e2) => Harf(harf: e2))],
-              ),
-            )
-          ]),
-          const SizedBox(
-            height: 10,
-          ),
-          Container(
-            padding: const EdgeInsets.all(5),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: oyunBitti && kazandiMi
-                  ? Theme.of(context).colorScheme.primary
-                  : Theme.of(context).colorScheme.error,
-            ),
-            child: Text(
-              mesaj,
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyMedium!
-                  .copyWith(color: Colors.white),
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Container(
-            padding: const EdgeInsets.all(5),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Expanded(
-                  child: TextField(
-                    onChanged: (word) {
-                      print(word);
-                      if (word.length == kelime.length) {
-                        tahminEdebilir = true;
-                      } else {
-                        tahminEdebilir = false;
-                      }
-                      setState(() {});
-                    },
-                    controller: tahminController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      contentPadding:
-                          const EdgeInsets.symmetric(horizontal: 10),
-                      hintText: "Tahmininizi buraya yazın...",
-                    ),
-                  ),
-                ),
-                Column(
-                  children: [
-                    Text("Hakkınız: $hak"),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: tahminEdebilir &&
-                                kalanSure > 0 &&
-                                hak > 0 &&
-                                !oyunBitti
-                            ? Theme.of(context).colorScheme.primary
-                            : Theme.of(context).colorScheme.error,
-                      ),
-                      child: IconButton(
-                        icon: const Icon(Icons.check),
-                        color: Colors.white,
-                        onPressed: tahminEdebilir &&
-                                kalanSure > 0 &&
-                                hak > 0 &&
-                                !oyunBitti
-                            ? () {
-                                tahmin(tahminController.text.toUpperCase());
-                                setState(() {
-                                  hak--;
-                                  kalanSure = orjinalSure;
-                                });
-                              }
-                            : null,
-                      ),
-                    ),
-                  ],
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.secondary,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: IconButton(
-                    color: Colors.white,
-                    icon: const Icon(Icons.refresh),
-                    onPressed: () {
-                      oyunuYenile();
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+    List kelime = yeniKelime().toUpperCase().split("");
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            timerController.restart();
+            setState(() {});
+          },
+          child: const Text("Yenile"),
+        ),
+        Oyun(
+          kelime: kelime,
+          timerController: timerController,
+        ),
+      ],
     );
   }
 }
--- lib/screen/oyun.dart
+++ lib/screen/oyun.dart
@@ -1,20 +1,113 @@
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
+import 'package:circular_countdown_timer/countdown_text_format.dart';
 import 'package:flutter/material.dart';
+import 'package:kelime_oyunu/widgets/harf_kutusu.dart';
+import 'package:timer_count_down/timer_controller.dart';
+import 'package:timer_count_down/timer_count_down.dart';
 
 class Oyun extends StatefulWidget {
-  const Oyun({super.key});
-
+  Oyun({super.key, required this.kelime, required this.timerController});
+  final List kelime;
+  final CountDownController timerController;
+  int hak = 4; // parametre olarak verilecek
+  int sure = 10;
+  List gorunenKelime = []; // parametre olarak verilecek
+  int tahminSayisi = 0; // parametre
+  List gorunenKelimeler = []; // parametre
+  String mesaj = "";
+  bool oyunBitti = false;
+  bool tahminEdebilir = false;
+  bool kazandiMi = false;
+  bool sureBitti = false;
+  TextEditingController tahminController = TextEditingController();
+  // final CountdownController timerController =
+  //     CountdownController(autoStart: true);
   @override
   State<Oyun> createState() => _OyunState();
 }
 
 class _OyunState extends State<Oyun> {
+  bool kaybettiMi() {
+    return widget.hak == 0 || widget.sureBitti;
+  }
+
+  void tahmin(String tahminKelime) {
+    print("tahmin edilen kelime $tahminKelime");
+    print("asıl kelime kelime ${widget.kelime.join()}");
+    for (int j = 0; j < widget.kelime.length; j++) {
+      if (widget.kelime[j] == tahminKelime[j]) {
+        widget.gorunenKelimeler[widget.tahminSayisi][j] = "+${tahminKelime[j]}";
+      } else {
+        widget.gorunenKelimeler[widget.tahminSayisi][j] = "-${tahminKelime[j]}";
+      }
+
+      if (widget.kelime.contains(tahminKelime[j]) &&
+          !widget.gorunenKelimeler[widget.tahminSayisi]
+              .contains("+${tahminKelime[j]}")) {
+        widget.gorunenKelimeler[widget.tahminSayisi][j] = "?${tahminKelime[j]}";
+      }
+    }
+
+    if (widget.kelime.join() == tahminKelime) {
+      widget.kazandiMi = true;
+    } else {
+      widget.timerController.restart();
+    }
+
+    widget.tahminSayisi++;
+    widget.tahminController.text = "";
+    widget.tahminEdebilir = false;
+  }
+
+  List gorunenKelimeOlustur(int uzunluk) {
+    List gorunenKelime = [];
+    for (int i = 0; i < uzunluk; i++) {
+      gorunenKelime.add("  ");
+    }
+    return gorunenKelime;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    // oyun sonucunu kontrol et
+    if (widget.kazandiMi) {
+      widget.mesaj = "Tebrikler";
+      widget.oyunBitti = true;
+    } else if (kaybettiMi()) {
+      print("oyunu kaybetti");
+      widget.mesaj = "KELİME ŞUYDU : ${widget.kelime.join()}";
+      widget.oyunBitti = true;
+      print("oyun bitti ${widget.oyunBitti}");
+    }
+    if (widget.oyunBitti) {
+      print("oyun bitti");
+      widget.timerController.pause();
+    }
+
+    if (widget.gorunenKelime.length != widget.kelime.length) {
+      widget.gorunenKelime = [widget.kelime[0]];
+      for (int i = 1; i < widget.kelime.length; i++) {
+        widget.gorunenKelime.add("  ");
+      }
+    }
+
+    if (widget.gorunenKelimeler.isEmpty) {
+      widget.gorunenKelimeler.add(widget.gorunenKelime);
+      for (int i = 1; i < widget.hak; i++) {
+        widget.gorunenKelimeler.add(gorunenKelimeOlustur(widget.kelime.length));
+      }
+    }
     return SingleChildScrollView(
       child: Column(
         children: [
           Column(children: [
-            ...gorunenKelimeler.map(
+            ...widget.gorunenKelimeler.map(
               (e) => Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [...e.map((e2) => Harf(harf: e2))],
@@ -24,25 +117,90 @@ class _OyunState extends State<Oyun> {
           const SizedBox(
             height: 10,
           ),
-          Container(
-            padding: const EdgeInsets.all(5),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: oyunBitti && kazandiMi
-                  ? Theme.of(context).colorScheme.primary
-                  : Theme.of(context).colorScheme.error,
-            ),
-            child: Text(
-              mesaj,
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyMedium!
-                  .copyWith(color: Colors.white),
-            ),
+          // Countdown(
+          //   controller: widget.timerController,
+          //   seconds: widget.sure,
+          //   build: (_, double time) => Text(
+          //     time.toString(),
+          //     style: const TextStyle(
+          //       fontSize: 20,
+          //     ),
+          //   ),
+          //   interval: const Duration(milliseconds: 100),
+          //   onFinished: () {
+          //     ScaffoldMessenger.of(context).showSnackBar(
+          //       const SnackBar(
+          //         content: Text('Timer is done!'),
+          //       ),
+          //     );
+          //   },
+          // ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: widget.oyunBitti && widget.kazandiMi
+                      ? Theme.of(context).colorScheme.primary
+                      : Theme.of(context).colorScheme.error,
+                ),
+                child: Text(
+                  widget.mesaj,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyMedium!
+                      .copyWith(color: Colors.white),
+                ),
+              ),
+              CircularCountDownTimer(
+                duration: widget.sure,
+                initialDuration: 0,
+                controller: widget.timerController,
+                width: 50,
+                height: 50,
+                ringColor: Colors.grey[300]!,
+                ringGradient: null,
+                fillColor: Theme.of(context).colorScheme.primary,
+                fillGradient: null,
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+                backgroundGradient: null,
+                strokeWidth: 20.0,
+                strokeCap: StrokeCap.round,
+                textStyle: const TextStyle(
+                    fontSize: 10.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+                textFormat: CountdownTextFormat.S,
+                isReverse: true,
+                isReverseAnimation: true,
+                isTimerTextShown: true,
+                autoStart: true,
+                onStart: () {
+                  debugPrint('Countdown Started');
+                },
+                onComplete: () {
+                  setState(() {
+                    widget.sureBitti = true;
+                  });
+                  debugPrint('Countdown Ended');
+                },
+                onChange: (String timeStamp) {
+                  debugPrint('Countdown Changed $timeStamp');
+                },
+                timeFormatterFunction: (defaultFormatterFunction, duration) {
+                  if (duration.inMilliseconds == 0) {
+                    return "Süre bitti";
+                  } else {
+                    return Function.apply(defaultFormatterFunction, [duration]);
+                  }
+                },
+              ),
+            ],
           ),
-          const SizedBox(
-            height: 10,
-          ),
+
           Container(
             padding: const EdgeInsets.all(5),
             child: Row(
@@ -53,14 +211,14 @@ class _OyunState extends State<Oyun> {
                   child: TextField(
                     onChanged: (word) {
                       print(word);
-                      if (word.length == kelime.length) {
-                        tahminEdebilir = true;
+                      if (word.length == widget.kelime.length) {
+                        widget.tahminEdebilir = true;
                       } else {
-                        tahminEdebilir = false;
+                        widget.tahminEdebilir = false;
                       }
                       setState(() {});
                     },
-                    controller: tahminController,
+                    controller: widget.tahminController,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
@@ -73,47 +231,33 @@ class _OyunState extends State<Oyun> {
                 ),
                 Column(
                   children: [
-                    Text("Hakkınız: $hak"),
+                    Text("Hakkınız: ${widget.hak}"),
                     Container(
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
-                        color: tahminEdebilir &&
-                                kalanSure > 0 &&
-                                hak > 0 &&
-                                !oyunBitti
+                        color: widget.tahminEdebilir &&
+                                widget.hak > 0 &&
+                                !widget.oyunBitti
                             ? Theme.of(context).colorScheme.primary
                             : Theme.of(context).colorScheme.error,
                       ),
                       child: IconButton(
                         icon: const Icon(Icons.check),
                         color: Colors.white,
-                        onPressed: tahminEdebilir &&
-                                kalanSure > 0 &&
-                                hak > 0 &&
-                                !oyunBitti
+                        onPressed: widget.tahminEdebilir &&
+                                widget.hak > 0 &&
+                                !widget.oyunBitti
                             ? () {
-                                tahmin(tahminController.text.toUpperCase());
+                                tahmin(
+                                    widget.tahminController.text.toUpperCase());
                                 setState(() {
-                                  hak--;
-                                  kalanSure = orjinalSure;
+                                  widget.hak--;
                                 });
                               }
                             : null,
                       ),
                     ),
                   ],
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.secondary,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: IconButton(
-                    color: Colors.white,
-                    icon: const Icon(Icons.refresh),
-                    onPressed: () {
-                      oyunuYenile();
-                    },
-                  ),
                 ),
               ],
             ),
